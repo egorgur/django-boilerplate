daadd1254185dd696d35d6ae3fe1be7ca7f43cc5
Bezizvestnyi <chikovm@bk.ru>
2024-09-17 10:58:46 +0700

    Make logs to store in the file
    Add functionality of saving logs of
    level "Warning" and higher into
    the file logs/debug.log

--- src/config/settings/logging.py
+++ src/config/settings/logging.py
@@ -1,25 +1,37 @@
 LOGGING = {
     "version": 1,
     "disable_existing_loggers": False,
-    "formatters": {"standard": {"format": "%(asctime)s - %(levelname)s - %(name)s %(message)s"}},
-    "filters": {},
+    "formatters": {
+        "standard": {
+            "format": "%(asctime)s %(levelname)s %(name)s %(message)s"
+        }
+    },
     "handlers": {
         "console": {
             "level": "INFO",
             "class": "logging.StreamHandler",
             "formatter": "standard",
             "filters": [],
-        }
+        },
+        "file": {
+            "level": "WARNING",
+            "class": "logging.FileHandler",
+            "filename": "logs/debug.log",
+            "encoding": "utf-8",
+            "formatter": "standard",
+            "filters": [],
+        },
     },
     "loggers": {
         logger_name: {
-            "level": "INFO",
+            "level": "WARNING",
             "propagate": True,
-        }
-        for logger_name in ("django", "django.request", "django.db.backends", "django.template", "src")
+            "handlers": ["file"],
+        } for logger_name in ("django", "django.request", "django.db.backends", "django.template", "src", "console")
     },
     "root": {
         "level": "DEBUG",
         "handlers": ["console"],
-    },
+    }
 }
+
