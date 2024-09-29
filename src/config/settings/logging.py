114be6b3798362f499f7f66ad6de09f7bf987edc
Bezizvestnyi <chikovm@bk.ru>
2024-09-29 14:01:54 +0700

    Since now logs record in a bit different way
    Add INFO filter for recording into debug.log file
    all levels of logs go to the console output

--- src/config/settings/logging.py
+++ src/config/settings/logging.py
@@ -1,22 +1,52 @@
+import logging
+import logging.config
+# use this - logging.basicConfig(level=logging.DEBUG), while writing your own logs to catch all the levels
+
+class InfoFilter(logging.Filter):
+    def filter(self, record):
+        return record.levelno == logging.INFO
+
+
 LOGGING = {
     "version": 1,
     "disable_existing_loggers": False,
-    "formatters": {"standard": {"format": "%(asctime)s - %(levelname)s - %(name)s %(message)s"}},
-    "filters": {},
+    "formatters": {
+        "standard": {
+            "format": "%(asctime)s %(levelname)s %(name)s %(message)s"
+        }
+    },
+
     "handlers": {
         "console": {
-            "class": "rich.logging.RichHandler",
+            "level": "INFO",
+            "class": "logging.StreamHandler",
+            "formatter": "standard",
+            "filters": []
+
+        },
+        "file": {
+            "level": "INFO",
+            "class": "logging.FileHandler",
+            "filename": "logs/debug.log",
+            "encoding": "utf-8",
+            "formatter": "standard",
+            "filters": ["info_filter"],
         },
     },
     "loggers": {
         logger_name: {
             "level": "INFO",
             "propagate": True,
+        } for logger_name in ("django", "django.request", "django.db.backends", "django.template", "src", "console")
+    },
+
+    "filters": {
+        "info_filter": {
+            "()":"src.config.settings.InfoFilter"
         }
-        for logger_name in ("django", "django.request", "django.db.backends", "django.template", "src")
     },
     "root": {
-        "handlers": ["console"],
+        "handlers": ["console", "file"],
         "level": "INFO",
     },
 }
